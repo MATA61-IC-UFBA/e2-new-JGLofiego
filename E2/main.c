f47de9858c879712ae808d57968cc9781b7380ad
João Gabriel <jg.lofiego@gmail.com>
2024-08-11 16:56:43 -0300

    update

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,21 @@
-/* main.c */
+#include <stdio.h>
+#include "token.h"
 
+extern int yylex();
+extern char *yytext;
+
+int main(){
+
+    token_t token = -1;
+    while(token != EOL){
+        token = yylex();
+
+        if(token == ERROR){
+            printf("Error on reading token %s\n", yytext);
+        } else if(token != NUMBER){
+            printf("codigo do token: %d\n", token);
+        } else {
+            printf("constante decimal: %s\n", yytext);
+        }
+    }
+}
